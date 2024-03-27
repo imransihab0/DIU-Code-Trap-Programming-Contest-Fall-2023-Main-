851d329f63087c29fc4ed411445d5d1bd53dd134
Imranul Islam Shihab <imran.sihab.0@gmail.com>
2024-03-27 19:04:16 +0600

    Add files via upload

---
+++ B_You_Are_Programmer_You_Have_No_Valentine!.cpp
@@ -0,0 +1,18 @@
+// https://toph.co/arena?practice=6603ff6f8bd03d06dab6bff1#!/p/65c5098f8ffc3901b49fecdb
+// @handle: imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define FastIO() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int32_t main(){
+    FastIO();
+    
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b << endl;
+    
+return 0;
+}
