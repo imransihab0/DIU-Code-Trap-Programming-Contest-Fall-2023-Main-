d7e0e9d6ef6a8037dad63f605b0ff6fcb3c62da6
Imranul Islam Shihab <imran.sihab.0@gmail.com>
2024-03-27 19:04:05 +0600

    Add files via upload

---
+++ A_Programming_is_the_key!.cpp
@@ -0,0 +1,15 @@
+// https://toph.co/arena?practice=6603ff6f8bd03d06dab6bff1#!/p/65c4c00de95ac323061eadfa
+// @handle: imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define FastIO() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int32_t main(){
+    FastIO();
+    
+    cout << "Programming is the key!" << endl;
+    
+return 0;
+}
