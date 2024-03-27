e61a1b167d611fc2ccf2f984d2ef4dbab3eefa4c
Imranul Islam Shihab <imran.sihab.0@gmail.com>
2024-03-27 19:08:50 +0600

    Add files via upload

---
+++ J Who Win the Game!!.cpp
@@ -0,0 +1,19 @@
+// https://toph.co/arena?practice=6603ff6f8bd03d06dab6bff1#!/p/65c4f6b6eca41135fba2adf2
+// @handle: imransihab0
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define FastIO() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int32_t main(){
+    FastIO();
+    
+    int a, b;
+    cin >> a >> b;
+
+    if(a > b) cout << "Alice" << endl;
+    else cout << "Bob" << endl;
+    
+return 0;
+}
